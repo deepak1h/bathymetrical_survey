dc9ba0a09fc0a107c6f4b38b8d8022d9fb7372ca
Deepak Pandey <43467891+zach-assassino@users.noreply.github.com>
2020-12-07 17:19:46 +0530

    csv and log generate file

---
+++ csv_data.py
@@ -0,0 +1,34 @@
+import main as project
+from threading import *
+import time
+import re
+import urllib3
+import json
+
+
+
+class csv(Thread):
+
+    def __init__(self):
+        Thread.__init__(self)
+        self.__csv_name = "/home/pi/Desktop/bathymetry_project/csv/" + time.asctime() + ".csv"
+        self.__old_data = "00,00,00,00"
+        self.__new_data = "00,00,00,00"
+
+    def put(self):
+        http = urllib3.PoolManager()
+
+        r = http.request('GET',
+                         "https://deepakhome.000webhostapp.com/updateacp.php?lat={}&lon={}&ele={}&dep={}".format(lat,long,ele,)                                                                                                      dep))
+        data = r.data.decode('utf-8')
+
+    def run(self):
+        while True:
+            self.__new_data = project.data
+            if self.__new_data == self.__old_data:
+                self.__csv_file = open(self.__csv_name,"a+")
+                self.__old_data = self.__new_data
+                self.__csv_file.write(self.__new_data)
+                self.__csv_file.close()
+            else:
+                pass
--- log.py
+++ log.py
@@ -5,3 +5,18 @@ class log(Thread):
 
     def __init__(self):
         Thread.__init__(self)
+        self.__log_name = "/home/pi/Desktop/bathymetry_project/log/" + time.asctime() + ".txt"
+        self.__old_time = []
+        self.__new_time = []
+
+    def run(self):
+        while True:
+            self.__new_time = project.time_update_log
+            if self.__new_time > self.__old_time:
+                self.__log_file = open(, self.__log_name,"a+")
+                self.__old_time = self.__new_time
+                self.__log_file.write(time.asctime().split()[3]+","+str(project.location))
+                self.__log_file.close()
+            else:
+                pass
+
